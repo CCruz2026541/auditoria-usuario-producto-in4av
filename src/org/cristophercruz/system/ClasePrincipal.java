e21019a0919100054a26db06762f67118ce0d069
Cristopher Cruz <ccruz-2026541@kinal.edu.gt>
2026-08-19 16:37:40 -0600

    feat: Se creo proyecto de login con FXML

---
+++ src/org/cristophercruz/system/ClasePrincipal.java
@@ -0,0 +1,20 @@
+package org.cristophercruz.system;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import org.cristophercruz.system.utils.SceneManager;
+import org.cristophercruz.system.utils.ViewFactory;
+
+public class ClasePrincipal extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage stageRoot) {
+        SceneManager.getInstanciaSceneManager().setStagePrincipal(stageRoot);
+        ViewFactory viewFacto = new ViewFactory();
+        viewFacto.viewLogin();
+    }
+}
---
+++ src/org/cristophercruz/system/model/User.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.cristophercruz.system.model;
+
+/**
+ *
+ * @author informatica
+ */
+public class User {
+    private String name;
+    private String lastName;
+    private String email;
+    private String user;
+    private String password;
+    private String idUser;
+
+    //Constructores
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+    
+    
+    
+    
+}
